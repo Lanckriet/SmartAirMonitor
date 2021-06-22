8f90c5f54b5568dcdbaad77f1fbb110c127cba27
Oliver Lanckriet <oliver.lanckriet@student.howest.be>
2021-06-22 19:49:07 +0200

    add co & pm ratings to js

--- public/js/main.js
+++ public/js/main.js
@@ -4,6 +4,7 @@ window.addEventListener("DOMContentLoaded", init);
 
 function init() {
     initializeMeasurements();
+    generateRatings();
 }
 
 // get all the data into their respective spans
@@ -12,16 +13,56 @@ function initializeMeasurements() {
     // update timestamp
     let timestamp = document.querySelector('#last-updated');
     timestamp.innerHTML = "01/01/1970 at 00:00";
+
     // temperature
     let temperature = document.querySelector('#temp');
     temperature.innerHTML = "21.6";
+
     // humidity
     let humidity = document.querySelector('#hum');
     humidity.innerHTML = "47";
+
     // carbon monoxide
     let carbonmonoxide = document.querySelector('#co');
-    carbonmonoxide.innerHTML = "28";
+    carbonmonoxide.innerHTML = "198";
+
     // particulate matter
     let particulatematter = document.querySelector('#pm');
     particulatematter.innerHTML = "16";
+}
+
+function generateRatings() {
+    let co = document.querySelector('#co').innerHTML;
+    let co_rating = document.querySelector('#co-rating');
+    let pm = document.querySelector('#pm').innerHTML;
+    let pm_rating = document.querySelector('#pm-rating');
+    switch(co_rating) {
+        case co < 100:
+            co_rating.innerHTML = "LOW";
+            break;
+        case co <= 500:
+            co_rating.innerHTML = "MODERATE";
+            break;
+        case co > 500:
+            co_rating.innerHTML = "HIGH";
+            break;
+        default:
+            co_rating.innerHTML = "LOW";
+    }
+
+    switch(co_rating) {
+        case co < 50:
+            co_rating.innerHTML = "LOW";
+            break;
+        case co <= 100:
+            co_rating.innerHTML = "MODERATE";
+            break;
+        case co > 100:
+            co_rating.innerHTML = "HIGH";
+            break;
+        default:
+            co_rating.innerHTML = "LOW";
+    }
+
+
 }
